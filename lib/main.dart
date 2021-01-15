13825d13ce1943c2d05e83ed97de3f6091aeab63
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-15 23:51:13 +0100

    split widgets and add scrollability

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expense_app/transaction.dart';
+import 'package:expense_app/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,27 +17,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly groceries',
-        amount: 16.53,
-        date: DateTime.now())
-  ];
-
-  // String titleInput;
-  // String amountInput;
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Flutter App')),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      appBar: AppBar(title: Text('Flutter App')),
+      body: SingleChildScrollView(
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           Container(
             width: double.infinity,
             child: Card(
@@ -47,67 +32,9 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      // onChanged: (value) {
-                      //   titleInput = value;
-                      // },
-                    ),
-                    TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController
-                        // onChanged: (value) => amountInput = value,
-                        ),
-                    FlatButton(
-                      child: Text('Add transaction'),
-                      textColor: Colors.purple,
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                    )
-                  ],
-                ),
-              )),
-          Column(
-              children: transactions
-                  .map((tx) => Card(
-                          child: Row(children: [
-                        Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 15),
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.purple, width: 2)),
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              '\$ ${tx.amount}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple),
-                            )),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                tx.title,
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              Text(DateFormat().format(tx.date),
-                                  style: TextStyle(color: Colors.grey))
-                            ])
-                      ])))
-                  .toList())
-        ]));
+          UserTransactions()
+        ]),
+      ),
+    );
   }
 }
