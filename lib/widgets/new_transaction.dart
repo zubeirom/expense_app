17d19e8a16b3ec37d51d6517d10394484c0d4cb0
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-16 17:26:04 +0100

    add date to data

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -17,14 +17,18 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime _selectedDate;
 
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.addTx(_titleController.text, double.parse(_amountController.text));
+    widget.addTx(_titleController.text, double.parse(_amountController.text),
+        _selectedDate);
 
     Navigator.of(context).pop();
   }
