9b11e7421f86410938500777c9cb427822c9e723
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-16 16:51:22 +0100

    different ui

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -25,38 +25,23 @@ class TransactionList extends StatelessWidget {
                 itemBuilder: (ctx, index) {
                   final currentTransaction = transactions[index];
                   return Card(
-                      child: Row(children: [
-                    Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$ ${currentTransaction.amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              fontFamily: 'Quicksand',
-                              color: Theme.of(context).primaryColor),
-                        )),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            currentTransaction.title,
-                            style: TextStyle(
-                                fontFamily: 'Quicksand',
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(DateFormat().format(currentTransaction.date),
-                              style: TextStyle(
-                                  color: Colors.grey, fontFamily: 'Quicksand'))
-                        ])
-                  ]));
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: FittedBox(
+                                child: Text('\$${currentTransaction.amount}')),
+                          )),
+                      title: Text(
+                        currentTransaction.title,
+                      ),
+                      subtitle: Text(
+                          DateFormat.yMMMd().format(currentTransaction.date)),
+                    ),
+                  );
                 },
                 itemCount: transactions.length,
               ));
